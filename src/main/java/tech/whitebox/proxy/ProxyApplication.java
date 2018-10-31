efa05403834b827b0b5e10bc65a8c2b23df8e533
Adrian Lambrache <adrian.lambrache@orange.com>
2018-10-31 18:27:37 +0200

    Fist Commit
    Add simple logger for requests

---
+++ src/main/java/tech/whitebox/proxy/ProxyApplication.java
@@ -0,0 +1,20 @@
+package tech.whitebox.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyApplication.class, args);
+	}
+
+    @Bean
+    public LoggingFilter simpleFilter() {
+        return new LoggingFilter();
+    }
+}
